e409e346c7a1ad0bfa619d3ae7557b9f6c6cac3b
Apoorv Gadiya <apoorvgadiya@gmail.com>
2024-04-15 21:16:30 +0530

    commit for main window

--- main/test.py
+++ main/test.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from tkinter import *
+import pyautogui
+
+window = Tk()
+
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+print(screen_height,screen_width)
+window.geometry(f"{screen_width}x{screen_height-125}+0+0")
+window.mainloop()
